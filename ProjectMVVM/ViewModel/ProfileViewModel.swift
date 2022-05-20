429cfe3d8786a954bce1156d9c3b7f28ea32bc26
LuchoMar123 <82741792+LuchoMar123@users.noreply.github.com>
2022-05-20 16:32:40 -0500

    Update ProfileViewModel.swift

--- ProjectMVVM/ViewModel/ProfileViewModel.swift
+++ ProjectMVVM/ViewModel/ProfileViewModel.swift
@@ -1,8 +1,6 @@
 import Foundation
 
 class ProfileViewModel {
-    //https://dummyapi.io/data/v1/user/60d0fe4f5311236168a10a0f
-    //app-id: 626c31c5df0af1efad6b2d4c}
     
     let URL_API = "https://dummyapi.io/data/v1/user/60d0fe4f5311236168a109ca"
     let appId = "626c31c5df0af1efad6b2d4c"
